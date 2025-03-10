d8c2962b65a671c162ba8442f63a57a955320e65
Kelsey Jane Adams <kelseyjaneadams3@gmail.com>
2025-03-10 16:30:13 +0000

    Create stateless function with props

---
+++ my-app/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import FunctionalGreetingWithProps from './components/FunctionalGreetingWithProps'
+
+function App() {
+  return (
+    <div className="App">
+     <FunctionalGreetingWithProps greeting="nice to meet you!" name="Mike" age="32"/>
+    </div>
+  );
+}
+
+export default App;
