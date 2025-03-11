12d4e27abe3e38983dd80624d9ac3a791ffcd014
Kelsey Jane Adams <kelseyjaneadams3@gmail.com>
2025-03-11 18:09:24 +0000

    Create stateful greeting with callback function and then another with prev state

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
 import FunctionalGreetingWithProps from './components/FunctionalGreetingWithProps'
+import StatefulGreeting from './components/StatefulGreeting';
+import StatefulGreetingWithCallback from './components/StatefulGreetingWithCallback';
+import StatefulGreetingWithPrevState from './components/StatefulGreetingWithPrevState';
 
 function App() {
   return (
     <div className="App">
      <FunctionalGreetingWithProps greeting="nice to meet you!" name="Mike" age="32"/>
+     <StatefulGreeting greeting="I'm a stateful class component" name="Mike"/>
+     <StatefulGreetingWithCallback greeting="I'm a stateful class component with callback" name="Mike"/>
+     <StatefulGreetingWithPrevState greeting="I'm a stateful class component with prev state" name="Mike"/>
     </div>
   );
 }
