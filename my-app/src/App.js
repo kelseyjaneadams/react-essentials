f1c0ec2ad035dfee5c9858709fb72c2308b2ade3
Kelsey Jane Adams <kelseyjaneadams3@gmail.com>
2025-03-13 17:41:28 +0000

    Update app.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -8,6 +8,7 @@ import FunctionalGreetingWithProps from './components/FunctionalGreetingWithProp
 import StatefulGreeting from './components/StatefulGreeting';
 import StatefulGreetingWithCallback from './components/StatefulGreetingWithCallback';
 import StatefulGreetingWithPrevState from './components/StatefulGreetingWithPrevState';
+import MethodsAsPropsParent from './components/MethodsAsPropsParent';
 
 
 function App() {
@@ -22,6 +23,7 @@ function App() {
      <ConditionalRenderingFunctional connected={true}/>
      <ConditionalRenderingClass/>
      <NestingComponents/>
+     <MethodsAsPropsParent/>
     </div>
   );
 }
